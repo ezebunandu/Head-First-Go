d9793d42b39b6e3198af78253fea87565c58dad5
Sam Ezebunandu <sam.ezebunandu@gmail.com>
2022-03-17 07:33:27 -0600

    Better error handling

--- datafile/floats.go
+++ datafile/floats.go
@@ -12,7 +12,7 @@ func GetFloats(fileName string) ([]float64, error) {
 	var numbers []float64
 	file, err := os.Open(fileName)
 	if err != nil {
-		return numbers, err
+		return nil, err
 	}
 
 	i := 0
@@ -20,17 +20,17 @@ func GetFloats(fileName string) ([]float64, error) {
 	for scanner.Scan() {
 		number, err := strconv.ParseFloat(scanner.Text(), 64)
 		if err != nil {
-			return numbers, err
+			return nil, err
 		}
 		numbers = append(numbers, number)
 		i++
 	}
 	err = file.Close()
 	if err != nil {
-		return numbers, err
+		return nil, err
 	}
 	if scanner.Err() != nil {
-		return numbers, scanner.Err()
+		return nil, scanner.Err()
 	}
 	return numbers, nil
 }
