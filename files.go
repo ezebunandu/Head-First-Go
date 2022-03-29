21cb6cf306e57f533de29f4fe75e07289868715b
Sam Ezebunandu <sam.ezebunandu@gmail.com>
2022-03-29 16:10:05 -0600

    using panic to improve error handling?

--- files.go
+++ files.go
@@ -3,34 +3,30 @@ package main
 import (
 	"fmt"
 	"io/ioutil"
-	"log"
 	"path/filepath"
 )
 
-func scanDirectory(path string) error {
+func scanDirectory(path string) { // error return value no longer needed
 	fmt.Println(path)
 	files, err := ioutil.ReadDir(path)
 	if err != nil {
-		return err
+		// instead of returning the value of the error value, pass it to "panic"
+		panic(err)
 	}
 
 	for _, file := range files {
 		filePath := filepath.Join(path, file.Name())
 		if file.IsDir() {
-			err := scanDirectory(filePath)
-			if err != nil {
-				return err
-			}
+			// no more need to store or check error return value
+			scanDirectory(filePath)
+
 		} else {
 			fmt.Println(filePath)
 		}
 	}
-	return nil
 }
 
 func main() {
-	err := scanDirectory(".")
-	if err != nil {
-		log.Fatal(err)
-	}
+	// no more need to store or check error return value
+	scanDirectory(".")
 }
